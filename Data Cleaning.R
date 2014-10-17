7113897001e683c33b6b3e1a1021283081e0b3d0
LBRETZIN <lbretzin@syr.edu>
2014-10-18 01:41:02 +0200

    Update Data Cleaning.R

--- Data Cleaning.R
+++ Data Cleaning.R
@@ -12,20 +12,6 @@
 ###### Cleaning Data  ######
 ############################
 
-# MPP-E1180: Introduction to Collaborative Social Science Data Analysis
-### Fall 2014
-### Instructor: Christopher Gandrud
-
-############################
-####### URBAN TERROR #######
-############################
-###### Part 1: Data   ######
-############################
-#Lukas B Cameron R Sascha S#
-############################
-###### Cleaning Data  ######
-############################
-
 #Load basic R packages
 library(foreign) 
 library(car)
