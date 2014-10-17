1ee31fca3221893363b37337e7e306cd0f144aeb
LBRETZIN <lbretzin@syr.edu>
2014-10-17 15:37:04 +0200

    Getting started with the empty Data Cleaning script file

---
+++ Data Cleaning.R
@@ -0,0 +1,13 @@
+# MPP-E1180: Introduction to Collaborative Social Science Data Analysis
+### Fall 2014
+### Instructor: Christopher Gandrud
+
+############################
+####### URBAN TERROR #######
+############################
+###### Part 1: Data   ######
+############################
+#Lukas B Cameron R Sascha S#
+############################
+###### Cleaning Data  ######
+############################
