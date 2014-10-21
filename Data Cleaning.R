37e04d37da9ac338cb7989f5a80fb0fa3703ba66
SaschaSchuster <saschust@gmail.com>
2014-10-21 17:18:01 +0200

    Put some spaces in it

--- Data Cleaning.R
+++ Data Cleaning.R
@@ -28,12 +28,20 @@ rawGTD <- read.csv("globalterrorismdb_0814dist.csv", header=TRUE)
 #We therefore filter the database to make it fit our needs. We erase over a 100 variables. 
 #We only want to look at successfull terror attacks and the ones that happened after 1989.
 
-GTD <- subset(rawGTD, select = c(eventid, iyear, imonth, iday, country, region, attacktype1, targtype1, targsubtype1, weaptype1, weapsubtype1, propextent, nkill, nwound), iyear >= 1989 & success == 1, na.strings = c("", " "))
+GTD <- subset(rawGTD, select = c(eventid, iyear, imonth, iday, country, region, attacktype1, targtype1, targsubtype1,
+                                weaptype1, weapsubtype1, propextent, nkill, nwound), 
+                                iyear >= 1989 & success == 1, na.strings = c("", " "))
 
 ### We introduce our first scale: "Targets Urbanity Potential Scale (TUPscale)" 
 GTD["TUPscale"] <- GTD$targsubtype1
-GTD$TUPscale <- recode(GTD$TUPscale, "40:42 = 3; 9 = 1; 27:35 = 1; 37:39 = 1; 65 = 1; 72 = 1; 1 = 2; 4:5 = 2; 10 = 2; 12 = 2; 53:56 = 2; 58:59 = 2; 61:62 = 2; 82 = 2; 95:96 = 2;6 = 3; 13 = 3; 104:108 = 3; 51:52 = 3; 57 = 3; 60 = 3; 63:64 = 3; 73 = 3; 80:81 = 3; 88:92 = 3; 98 = 3; 2 = 4; 3 = 4; 7:8 = 4; 44 = 4;  48:50 = 4; 67:71 = 4; 74:79 = 4; 83:87 = 4; 97 = 4; 99 = 4; 14:26 = 5; 100:103 = 5; 111 = 5; 109 = 5; 110 = 5; 36 = 5; 43 = 5; 45:47 = 5; 66 = 5; 93:94 = 5")
-# 1= Rural & Military; 2= Government & Police; 3= Potentilly Urban Workplace; 4= Potentilly Urban Infrastructure; 5= Potentilly Urban Core Life 
+GTD$TUPscale <- recode(GTD$TUPscale, "40:42 = 3; 9 = 1; 27:35 = 1; 37:39 = 1; 65 = 1; 72 = 1; 1 = 2; 4:5 = 2; 10 = 2; 
+                                      12 = 2; 53:56 = 2; 58:59 = 2; 61:62 = 2; 82 = 2; 95:96 = 2;6 = 3; 13 = 3;
+                                      104:108 = 3; 51:52 = 3; 57 = 3; 60 = 3; 63:64 = 3; 73 = 3; 80:81 = 3; 88:92 = 3;
+                                      98 = 3; 2 = 4; 3 = 4; 7:8 = 4; 44 = 4;  48:50 = 4; 67:71 = 4; 74:79 = 4; 83:87 = 4;
+                                      97 = 4; 99 = 4; 14:26 = 5; 100:103 = 5; 111 = 5; 109 = 5; 110 = 5; 36 = 5; 43 = 5; 
+                                      45:47 = 5; 66 = 5; 93:94 = 5")
+# 1= Rural & Military; 2= Government & Police; 3= Potentilly Urban Workplace; 
+#4= Potentilly Urban Infrastructure; 5= Potentilly Urban Core Life 
 
 GTD$TUPscale <- as.numeric(GTD$TUPscale)
 #Bring TUPscale down to values between 0 and 1 that reflect our appreciation for the targets potential urbanity.
@@ -63,7 +71,8 @@ TUPxDAMAGE <-TUP_weight_as_DAMAGE_0_up_to_1*DAMAGE_0_up_to_1
 TUPandDAMAGE <-TUP_weight_as_DAMAGE_0_up_to_1+DAMAGE_0_up_to_1
 
 # make dataframe for plotting
-Plotframe1 <- data.frame(year=GTD$iyear, tup= TUP_weight_as_DAMAGE_0_up_to_1, dam= DAMAGE_0_up_to_1, tupxdamage= TUPxDAMAGE, tupanddamage= TUPandDAMAGE, region= GTD$region)
+Plotframe1 <- data.frame(year=GTD$iyear, tup= TUP_weight_as_DAMAGE_0_up_to_1, dam= DAMAGE_0_up_to_1, 
+                          tupxdamage= TUPxDAMAGE, tupanddamage= TUPandDAMAGE, region= GTD$region)
 
 
 ##!!!!!!!##########plots dont produce trendline########!!!!!!!!##
