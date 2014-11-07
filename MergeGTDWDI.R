4c612b41b3f65fc1f60d4a5a00559d0a8cde8521
SaschaSchuster <saschust@gmail.com>
2014-11-07 14:25:11 +0100

    Update MergeGTDWDI.R

--- MergeGTDWDI.R
+++ MergeGTDWDI.R
@@ -3,10 +3,10 @@
 
 #We need to figure out an elegenat way to rename columns. As long as we can't fix this (plyr's rename does not seem to work), 
 #we add a new column and delete the old one.
-WDIUrbanDat["country_txt"] <- WDIUrbanDat$country
-WDIUrbanDat$country <- NULL
-WDIUrbanDat["iyear"] <- WDIUrbanDat$year
-WDIUrbanDat$year <- NULL
+WDIData["country_txt"] <- WDIData$country
+WDIData$country <- NULL
+WDIData["iyear"] <- WDIData$year
+WDIData$year <- NULL
 
-GTDWDI <-merge(GTD, WDIUrbanDat, by=c("country_txt", "iyear"), all.x = TRUE , sort=TRUE, suffixes=c(".G", ".W"))
+GTDWDI <-merge(GTD, WDIData, by=c("country_txt", "iyear"), all.x = TRUE , sort=TRUE, suffixes=c(".G", ".W"))
 
