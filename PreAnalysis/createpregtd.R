748bc013d0c0b6c9e70b241b0adb16ff6cde3fe4
LBRETZIN <lbretzin@syr.edu>
2014-11-11 13:13:40 +0100

    up

--- PreAnalysis/createpregtd.R
+++ PreAnalysis/createpregtd.R
@@ -1,4 +1,4 @@
-#testbed
+#testbed to write a pre-gtd
 X <- GTD$city
 source('CityCleaning.R')
 X <-gsub("NA", NA, X)
@@ -15,19 +15,13 @@ X <- gsub("\\'","",X)
 X <- gsub("\\-","",X)
 t.world.cities$country.etc<- gsub("\\'","",t.world.cities$country.etc)
 t.world.cities$name<-X
-
 world.cities$CityID <- NULL
-
 Testframe <- GTD[1|2|9|11:20]
 Testframe["merge"] <-data.frame(paste(GTDcountry, GTDcity, sep=""))
 WC09.UCdist$merge <- paste(WC09.UCdist$country.etc, WC09.UCdist$name, sep="")
-
 WC09.UCdist$merge <- gsub("\\-","",t.world.cities$merge)
 WC09.UCdist$merge <- gsub("\\'","",t.world.cities$merge)
-
 PreGTD <- merge(Testframe, WC09.UCdist, by=c("merge"), all.x=TRUE)
-
-
 PreGTD$merge <- NULL
 PreGTD$iday <- NULL
 PreGTD$country <- NULL
@@ -41,6 +35,4 @@ PreGTD$lat<- NULL
 PreGTD$long<- NULL
 PreGTD$Region<- NULL
 PreGTD$country.etc<- NULL
-
-
 write.csv(PreGTD, file="pregtd.csv")
