020dced4b162c677afd6832d5a6ba005102ed3aa
LBRETZIN <lbretzin@syr.edu>
2014-11-13 17:34:39 +0100

    Update createpregtd.R

--- PreAnalysis/createpregtd.R
+++ PreAnalysis/createpregtd.R
@@ -27,12 +27,14 @@ Testframe["merge"] <-data.frame(paste(GTDcountry, GTDcity, sep=""))
 PreGTD <- merge(Testframe, world.citiesUC, by=c("merge"), all.x=TRUE)
 PreGTD  <- PreGTD [order(-PreGTD$eventid, na.last=TRUE) , ]
 
-PreGTD <- subset(PreGTD, select=c(eventid, merge, iyear, region_txt, city, pop,capital, Closest.Urban.Center, 
+PreGTD <- subset(PreGTD, select=c(eventid, merge, iyear, region_txt, city, pop, capital, Closest.Urban.Center, 
                                   CUC.dist.km, part.of.urban.center, in.urban.centers.environment, attacktype1, 
                                   targtype1, targsubtype1, weaptype1, weapsubtype1, TUPscale, PROPscale, HUMscale,
                                   EN.URB.LCTY.UR.ZS, EN.URB.MCTY, EN.URB.MCTY.TL.ZS, SP.URB.GROW, SP.URB.TOTL, 
                                   SP.URB.TOTL.IN.ZS, EN.POP.DNST, EN.RUR.DNST, SP.RUR.TOTL, SP.RUR.TOTL.ZG, SP.RUR.TOTL.ZS))
 
+#> sum(is.na(PreGTD$pop))
+#[1] 
 
 write.csv(PreGTD, file="PreAnalysis/pregtd.csv")
 rm(Testframe, GTDcity, GTDcountry, X, Cities, Countries)
