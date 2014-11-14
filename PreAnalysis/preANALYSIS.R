8f49ea2b285e207d3e8118e1a85ffab87065e978
Leonardo2011 <c.reed@mpp.hertie-school.org>
2014-11-14 19:09:40 +0100

    some tables

--- PreAnalysis/preANALYSIS.R
+++ PreAnalysis/preANALYSIS.R
@@ -20,3 +20,29 @@ rm(PreGTD_in_Memory)
 #Load the Pre-Analysis Global Terrorism Database
 PreGTD <- read.csv("Pre.GTD.csv", header=TRUE)
 PreGTD <-PreGTD[order(-PreGTD$eventid, na.last=TRUE) , ]
+
+# make numeric what we need
+PreGTD$EN.URB.LCTY.UR.ZS <- as.numeric(PreGTD$EN.URB.LCTY.UR.ZS)
+PreGTD$SP.URB.TOTL <- as.numeric(PreGTD$SP.URB.TOTL)
+PreGTD$SP.RUR.TOTL <- as.numeric(PreGTD$SP.RUR.TOTL)
+PreGTD$MAX.URB.TOTL <- as.numeric(PreGTD$MAX.URB.TOTL)
+PreGTD$pop <- as.numeric(PreGTD$pop)
+
+# take a look at the structure of our data
+str(PreGTD)
+
+# make a table of attacks in capital cities per year
+with(PreGTD, table(iyear, capital))
+table(PreGTD$iyear, PreGTD$capital)
+with(PreGTD, table(iyear, (capital))) -> cap.table
+summary(cap.table)
+
+
+# some plots
+mosaicplot(cap.table, main="Attacks in Capital Cities", xlab="Year", ylab="Capital Cities (Sum)")
+mosaicplot(cap.table, sort=c(2,1))
+barplot(cap.table, legend=T, main="Attacks in Capital Cities")
+plot(cap.table)
+
+
+# merge in a coastal city vector
