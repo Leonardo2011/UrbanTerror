a347a7c0784c90b298c3d2683995ea20ce458450
Leonardo2011 <c.reed@mpp.hertie-school.org>
2014-12-04 14:55:54 +0100

    srilanka

---
+++ mapsrilanka.R
@@ -0,0 +1,83 @@
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg))
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+
+packages <- c("foreign", "car", "RCurl", "ggplot2", "WDI", "httr", "iterators", "dplyr", "plyr", "stargazer",
+              "XML", "maps", "ggmap", "Imap", "geosphere", "maptools", "rgeos", "foreach", "DataCombine")
+ipak(packages)
+rm(packages)
+rm(ipak)
+
+#download a limited GTD we created for this assignment (5MB instead of 100MB file)
+PreGTD_in_Memory <- getURL("https://rawgit.com/LBRETZIN/UrbanTerror/master/TerrorData/Pregtd.csv", ssl.verifypeer=0L, followlocation=1L)
+writeLines(PreGTD_in_Memory,'Pre.GTD.csv')
+rm(PreGTD_in_Memory)
+PreGTD <- read.csv("Pre.GTD.csv", header=TRUE)
+
+WC.UC.dist_in_Memory <- getURL("https://rawgit.com/LBRETZIN/UrbanTerror/master/Cache/WC.UC.dist.csv", ssl.verifypeer=0L, followlocation=1L)
+writeLines(WC.UC.dist_in_Memory,'WC.UC.dist.csv')
+rm(WC.UC.dist_in_Memory)
+WC.UC.dist <- read.csv("WC.UC.dist.csv", header=TRUE)
+
+install.packages("ggmap")
+install.packages("mapproj")
+
+library(ggmap)
+library(mapproj)
+
+
+#find map of sri lanka
+qmap('Sri Lanka', zoom = 8)
+
+#subset the database
+srilankamap <- subset(PreGTD, country_txt=="srilanka" & !is.na(latitude))
+srilankamap <- srilankamap[!duplicated(srilankamap$merge),]
+
+slmap <- qmap('Sri Lanka', zoom = 8,  
+                   source = "stamen", maptype = "toner",
+                   legend = "topleft", legend="none")
+
+# Add points
+FinalMap <- slmap + geom_point(aes(x = longitude, y = latitude),
+             data = srilankamap))
+
+
+overlay <- 
+
+
+print(FinalMap)
+
+
+
+
+
+
+
+
+
+
+
+##Extra###
+# only violent crimes
+violent_crimes <- subset(crime,
+                         offense != "auto theft" & offense != "theft" &
+                           offense != "burglary")
+
+# order violent crimes
+violent_crimes$offense <- factor(violent_crimes$offense,
+                                 levels = c("robbery", "aggravated assault", "rape", "murder"))
+# Set up base map
+HoustonMap <- qmap("houston", zoom = 14,  
+                   source = "stamen", maptype = "toner",
+                   legend = "topleft")
+
+# Add points
+FinalMap <- HoustonMap +
+  geom_point(aes(x = lon, y = lat, colour = offense,
+                 size = offense),
+             data = violent_crimes) +
+  guides(size = guide_legend(title = 'Offense'),
+         colour = guide_legend(title = 'Offense'))
