147a0840e2642226fc2676f9895f8fccf6494790
SaschaSchuster <saschust@gmail.com>
2014-12-12 17:00:04 +0100

    Map Visualisation Turkey

---
+++ AttackVis.R
@@ -0,0 +1,177 @@
+# This script visualizes our Data Gathering, Merging and Manipulating Process with the example of Turkey
+source("0 - Loading Packages.R")
+PreGTD <- read.csv("TerrorData/pregtd.csv")
+
+#Finding 2 basic maps of Turkey for visualisation
+Turkey <- qmap("Davulga", zoom = 7, extent = "device", legend = "topleft")
+BaseMap <- qmap("Davulga", zoom = 7, extent = "device", source="stamen", maptype="toner")
+
+#Creating our PreGTD subset with example attacks
+ExA <- subset(PreGTD, eventid == 200807270020 | eventid == 200608280001 | eventid == 199811270002 |
+                      eventid == 200608280002 | eventid == 199903050002 | eventid == 200308010004 | 
+                      eventid == 199907300003 | eventid == 199809090001 | 
+                      eventid == 199910180002 | eventid == 200311140004 )
+ExA$inUC <- as.factor(ExA$inUC)
+ExA$latitude <- as.numeric(ExA$latitude)
+ExA$longitude <- as.numeric(ExA$longitude)
+ExA$Relative Light <- ExA$light/ExA$light.MAX
+
+# Formatting the table so it does look nicely in Rmarkdwon and represents the right things
+Rmdattacks <- subset(ExA, select = c(Date, inUC, HUMscale, access, access.MAX, light, light.MAX, density, density.MAX, coast.dist, coast.dist.MAX, original.city, latitude))
+#ordering attacks from North to South
+Rmdattacks <- Rmdattacks[order(-Rmdattacks$latitude), ]
+
+Rmdattacks$inUC <- as.character(Rmdattacks$inUC)
+Rmdattacks$inUC[Rmdattacks$inUC == 0] <- "no"
+Rmdattacks$inUC[Rmdattacks$inUC == 1] <- "yes"
+
+Rmdattacks$Rel.Access <- round((((Rmdattacks$access/Rmdattacks$access.MAX)-1)*-100), digits = 2 )
+Rmdattacks$Rel.Light <- round(((Rmdattacks$light/Rmdattacks$light.MAX)*100), digits = 2)
+Rmdattacks$Rel.Density <- round(((Rmdattacks$density/Rmdattacks$density.MAX)*100), digits = 2)
+Rmdattacks$Rel.Coast.Dist <- round((((Rmdattacks$coast.dist/Rmdattacks$coast.dist.MAX)-1)*-100), digits = 2)
+Rmdattacks$Date.Place <- paste(Rmdattacks$Date, Rmdattacks$original.city, sep=", ")
+Rmdattacks$latitude <- NULL
+Rmdattacks$access <- NULL
+Rmdattacks$access.MAX <- NULL
+Rmdattacks$density <- NULL
+Rmdattacks$density.MAX <- NULL
+Rmdattacks$light <- NULL
+Rmdattacks$light.MAX <- NULL
+Rmdattacks$coast.dist <- NULL
+Rmdattacks$coast.dist.MAX <- NULL
+Rmdattacks$Date <- NULL
+Rmdattacks$original.city <- NULL
+colnames(Rmdattacks) <- c("On UC", "Killed & Wounded", "Rel. Access", "Rel. Light", "Rel. Density", "Rel. Prox. Coast", "Date & Place")
+Rmdattacks <- Rmdattacks[c(7,1,2,4,3,6,5) ]
+
+
+# show example attacks on map with distinginishing if take place on UC and labels
+AttackMap <- BaseMap + geom_point(aes(x=longitude, y=latitude, shape = inUC, color = inUC), data = ExA, size = 7 ) + 
+             theme(legend.position="none")+
+             geom_text(aes(x=longitude+0.3, y=latitude+0.08, fontface="bold", color = inUC, fontsize = 7), data=ExA, label=ExA$Date) + 
+             scale_colour_manual(values = c("firebrick","pink"))
+
+# Show lightmap
+unzip("Downloaded_Data/Downloaded_Raster_Data.zip", exdir="Downloaded_Data")
+unlink("Downloaded_Data/NLDI_2006_0p25_rev20111230.tif")
+unlink("Downloaded_Data/GDP_grid_flt.tif")
+unlink("Downloaded_Data/DICGSH1a.tif")
+unlink("Downloaded_Data/GACGEM2a.tif")
+unlink("Downloaded_Data/G19ESA3a.tif")
+RASTERlight<- raster("Downloaded_Data/LNMDMS2a.tif")
+#crop Raster to Turkey maps boundaries
+e <- extent(27.9, 34.9, 36.2, 41.7)
+LightLayer <- crop(RASTERlight, e)
+#convert to usable dataframe including eliminating minor light values and aggregating the rest from 63 to only 6 levels
+LightLayer <- as(LightLayer, "SpatialPixelsDataFrame")
+LightLayer <- data.frame(LightLayer)
+rm(RASTERlight, e)
+unlink("Downloaded_Data/LNMDMS2a.tif")
+colnames(LightLayer) <- c("light", "longitude", "latitude")
+LightLayer$light <- as.numeric(LightLayer$light)
+LightLayer <- subset(LightLayer, light > 3)
+LightLayer$light <- recode (LightLayer$light, "4:10 = 1; 11:20 = 2; 21:30 = 3; 31:40 = 4; 41:50 = 5; 51:63 = 6", as.numeric.result=TRUE)
+
+LightMap <- BaseMap + geom_tile(aes(x = longitude, y = latitude, fill=light), data = LightLayer, alpha = 0.7) +
+            geom_point(aes(x=longitude, y=latitude, shape = inUC, color = inUC), data = ExA, size = 7 ) +
+            scale_fill_gradient(low = "darkblue", high= "lightblue") +
+            theme(legend.position="none") + 
+            geom_text(aes(x=longitude+0.3, y=latitude+0.08, fontface="bold", color = inUC, fontsize = 7), data=ExA, label=ExA$Date) +
+            scale_colour_manual(values = c("firebrick","pink"))
+rm(LightLayer)
+
+# Show Accessmap
+unzip("Downloaded_Data/Downloaded_Raster_Data.zip", exdir="Downloaded_Data")
+unlink("Downloaded_Data/NLDI_2006_0p25_rev20111230.tif")
+unlink("Downloaded_Data/GDP_grid_flt.tif")
+unlink("Downloaded_Data/DICGSH1a.tif")
+unlink("Downloaded_Data/G19ESA3a.tif")
+unlink("Downloaded_Data/LNMDMS2a.tif")
+RASTERAccess<- raster("Downloaded_Data/GACGEM2a.tif")
+#crop Raster to Turkey maps boundaries
+e <- extent(27.9, 34.9, 36.2, 41.7)
+AccessLayer <- crop(RASTERAccess, e)
+#aggregating cells because else, way too muh values
+AccessLayer <- raster::aggregate(AccessLayer, fact=2, fun=mean, na.rm=TRUE)
+#convert to usable dataframe including eliminating largest Access time values (in minutes ) and aggregating the rest to 10 min intervals
+AccessLayer <- as(AccessLayer, "SpatialPixelsDataFrame")
+AccessLayer <- data.frame(AccessLayer)
+unlink("Downloaded_Data/GACGEM2a.tif")
+
+rm(RASTERAccess, e)
+
+colnames(AccessLayer) <- c("AccessTime", "longitude", "latitude")
+AccessLayer$AccessTime <- as.numeric(AccessLayer$AccessTime)
+
+#subsetting and binning Access Time values to 10 minute bins to reduce observations. N
+#ow, only displaying areas that have Access Time < 120 minutes
+AccessLayer <- subset(AccessLayer, AccessTime < 120)
+AccessLayer$AccessTime <- AccessLayer$AccessTime/10
+AccessLayer$AccessTime <- round(AccessLayer$AccessTime)
+
+AccessMap <- BaseMap + geom_tile(aes(x = longitude, y = latitude, fill=AccessTime), data = AccessLayer) +
+            geom_point(aes(x=longitude, y=latitude, shape = inUC, color = inUC), data = ExA, size = 7 ) +
+            scale_fill_gradient(low = "lightblue", high= "darkblue") +
+            theme(legend.position="none") + 
+            geom_text(aes(x=longitude+0.3, y=latitude+0.08, fontface="bold", color = inUC, fontsize = 7), data=ExA, label=ExA$Date) +
+            scale_colour_manual(values = c("firebrick","pink"))
+
+# Proximity to Coaast
+
+unzip("Downloaded_Data/Downloaded_Raster_Data.zip", exdir="Downloaded_Data")
+unlink("Downloaded_Data/NLDI_2006_0p25_rev20111230.tif")
+unlink("Downloaded_Data/GDP_grid_flt.tif")
+unlink("Downloaded_Data/GACGEM2a.tif")
+unlink("Downloaded_Data/G19ESA3a.tif")
+unlink("Downloaded_Data/LNMDMS2a.tif")
+
+RASTERCoast<- raster("Downloaded_Data/DICGSH1a.tif")
+
+#crop Raster to Turkey maps boundaries
+e <- extent(27.9, 34.9, 36.2, 41.7)
+CoastLayer <- crop(RASTERCoast, e)
+
+CoastLayer <- as(CoastLayer, "SpatialPixelsDataFrame")
+CoastLayer <- data.frame(CoastLayer)
+
+rm(RASTERCoast, e)
+unlink("Downloaded_Data/DICGSH1a.tif")
+
+colnames(CoastLayer) <- c("Coastdist", "longitude", "latitude")
+CoastLayer$Coastdist <- as.numeric(CoastLayer$Coastdist)
+#reducing No of observations to on-coast and less than a 100 km from coast
+CoastLayer <- subset(CoastLayer, Coastdist <=0 & Coastdist >=-100)
+CoastLayer$Coastdist <- CoastLayer$Coastdist*-1
+
+
+CoastMap <- BaseMap + geom_tile(aes(x = longitude, y = latitude, fill=Coastdist), data = CoastLayer, alpha=0.7) +
+            geom_point(aes(x=longitude, y=latitude, shape = inUC, color = inUC), data = ExA, size = 7 ) +
+            scale_fill_gradient(low = "lightblue", high= "darkblue") +
+            theme(legend.position="none") + 
+            geom_text(aes(x=longitude+0.3, y=latitude+0.08, fontface="bold", color = inUC, fontsize = 7), data=ExA, label=ExA$Date) +
+            scale_colour_manual(values = c("firebrick","pink"))
+
+
+
+# Population Density
+# Data sourced from http://neo.sci.gsfc.nasa.gov/servlet/RenderData?si=875430&cs=rgb&format=TIFF&width=3600&height=1800 and saved to Downloaded_Data
+RASTERDens <- raster("Downloaded_Data/SEDAC_POP_2000-01-01_rgb_3600x1800.TIFF")
+#crop Raster to Turkey maps boundaries
+e <- extent(27.9, 34.9, 36.2, 41.7)
+DensLayer <- crop(RASTERDens, e)
+
+DensLayer <- as(DensLayer, "SpatialPixelsDataFrame")
+DensLayer <- data.frame(DensLayer)
+
+rm(RASTERDens, e)
+
+colnames(DensLayer) <- c("Density", "longitude", "latitude")
+DensLayer$Density <- as.numeric(DensLayer$Density)
+#For whatever reason 255 is like NA, e.g. the sea
+DensLayer <- subset(DensLayer, Density <255)
+DensMap <- BaseMap + geom_tile(aes(x = longitude, y = latitude-0.1, fill=Density), data = DensLayer, alpha=0.7) +
+           geom_point(aes(x=longitude, y=latitude, shape = inUC, color = inUC), data = ExA, size = 7 ) + 
+           scale_fill_gradient(low = "lightblue", high= "darkblue") +
+           theme(legend.position="none") + 
+           geom_text(aes(x=longitude+0.3, y=latitude+0.08, fontface="bold", color = inUC, fontsize = 7), data=ExA, label=ExA$Date) +
+           scale_colour_manual(values = c("firebrick","pink")
